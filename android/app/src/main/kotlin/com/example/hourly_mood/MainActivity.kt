2dda888a401e21cb2bcbe5e5a2af41db8a853225
JackRWilson <jackwilson4235@gmail.com>
2026-06-05 19:38:43 -0500

    Initial Flutter commit

---
+++ android/app/src/main/kotlin/com/example/hourly_mood/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hourly_mood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test/widget_test.dart
@@ -0,0 +1,2 @@
+// We'll add real tests later. This just stops the error.
+void main() {}
